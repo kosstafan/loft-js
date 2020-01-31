25071c28333d42e529c8d3ca9fa7df66aa3092a6
kosstafan <kostafan_roc@list.ru>
2020-01-31 16:39:09 +0500

    hw-1 done

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {
+function returnFirstArgument(param) {
+    return param;
 }
 
 /*
@@ -30,6 +31,11 @@ function returnFirstArgument() {
    sumWithDefaults(10) вернет 110
  */
 function sumWithDefaults(a, b) {
+    if (!b) {
+        b = 100;
+    }
+
+    return a + b;
 }
 
 /*
@@ -41,6 +47,7 @@ function sumWithDefaults(a, b) {
    returnFnResult(() => 'привет') вернет 'привет'
  */
 function returnFnResult(fn) {
+    return fn()
 }
 
 /*
@@ -57,6 +64,15 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 13
  */
 function returnCounter(number) {
+    if (!number) {
+        number = 0;
+    }
+
+    return function () {
+        number ++;
+
+        return number;
+    }
 }
 
 /*
@@ -69,6 +85,13 @@ function returnCounter(number) {
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
 function returnArgumentsArray() {
+    let allArgs = [];
+
+    for (let i = 0; i < arguments.length; i++) {
+        allArgs.push(arguments[i]);
+    }
+
+    return allArgs;
 }
 
 /*
@@ -86,7 +109,8 @@ function returnArgumentsArray() {
 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn) {
+function bindFunction(fn, ...args) {
+    return fn.bind(null, ...args);
 }
 
 export {
